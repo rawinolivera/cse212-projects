dafefedea5b86c2a674cf116e49b201545f73388
rawinolivera <oli18001@byui.edu>
2025-01-25 20:44:10 -0300

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,11 +9,22 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
+        // I will create an array and insert the number in the first position array[0]
+        //  I will define a for look to continue adding the elements to the array
+        // I will multiply the number by the current iteration plus 1 so that I do not insert the initial number twice
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        double[] array = new double[length];
+        array[0] = number;
+        for (int i = 1; i < length; i++)
+        {
+            array[i] = number * (i + 1);
+        }
+
+
+        return array; // replace this return statement with your own
     }
 
     /// <summary>
@@ -26,8 +37,22 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
+        // I will define a variable lenght to get the Count of the list and create a for loop to handle the rotations
+        // inside the for I will get the last position
+        // I will save the value in a variable and delete from the list using Remove
+        // then I will use the function myList.Insert(index, value) to add it in the beginning of the list
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        int item = 0;
+        int lenght = data.Count;
+        for (int i = 0; i < amount; i++)
+        {
+            item = data[lenght - 1];
+            data.Remove(data[lenght - 1]);
+            data.Insert(0, item);
+        }
+
+
     }
 }
